6a4c03985d11c69f0aa93e59a692dfbcb1f7b025
L_B__ <2394626220@qq.com>
2023-04-28 20:57:59 +0800

    fix some log level

--- examples/echo_client_example.cc
+++ examples/echo_client_example.cc
@@ -6,37 +6,38 @@ using namespace elog;
 
 std::string input()
 {
-   std::cout << "please enter you message:";
-   std::string ret;
-   std::cin >> ret;
-   return ret;
+	std::cout << "please enter you message:";
+	std::string ret;
+	std::cin >> ret;
+	return ret;
 }
 
 struct client
 {
-   NETPOLL_TCP_CONNECTION(conn)
-   {
-      if (conn->connected()) { conn->send(input()); }
-   }
-   NETPOLL_TCP_MESSAGE(conn, buffer)
-   {
-      ELG_INFO("msg received:{}", buffer->readAll());
-      conn->send(input());
-   }
+	NETPOLL_TCP_CONNECTION(conn)
+	{
+		if (conn->connected())
+		{ conn->send(input()); }
+	}
+	NETPOLL_TCP_MESSAGE(conn, buffer)
+	{
+		ELG_INFO("msg received:{}", buffer->readAll());
+		conn->send(input());
+	}
 };
 
 int main()
 {
-   elog::GlobalConfig::Get().setLevel(elog::kTrace);
-   auto loop   = netpoll::NewEventLoop(1);
-   auto dialer = netpoll::tcp::Dialer::New({"172.30.209.225", 6666});
-   dialer->bind<client>();
+	elog::GlobalConfig::Get().setLevel(elog::kTrace);
+	auto loop = netpoll::NewEventLoop(1);
+	auto dialer = netpoll::tcp::Dialer::New({ "127.0.0.1", 6666 });
+	dialer->bind<client>();
 #if __cplusplus >= 201703L || (_MSC_VER && _MSVC_LANG >= 201703L)
 #else
-   netpoll::tcp::Dialer::Register<client>();
-   dialer->bindOnMessage<client>();
-   dialer->bindOnConnection<client>();
-   dialer->bindOnConnectionError<client>();
+	netpoll::tcp::Dialer::Register<client>();
+	dialer->bindOnMessage<client>();
+	dialer->bindOnConnection<client>();
+	dialer->bindOnConnectionError<client>();
 #endif
-   loop.serve(dialer);
+	loop.serve(dialer);
 }
