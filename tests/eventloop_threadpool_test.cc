6722bea8ee19751e5037b555baa98607be8fabbc
root <2394626220@qq.com>
2023-12-01 03:30:00 +0800

    fix lifetime error

--- tests/eventloop_threadpool_test.cc
+++ tests/eventloop_threadpool_test.cc
@@ -5,10 +5,11 @@
 #include <netpoll/util/defer_call.h>
 using namespace netpoll;
 using namespace elog;
+const int s_thread_num = 8;
 
 TEST_CASE("test EventLoopThreadPool")
 {
-   EventLoopThreadPool threadPool(8);
+   EventLoopThreadPool threadPool(s_thread_num);
    GlobalConfig::Get().setFormatter(formatter::customFromString("%v"));
    threadPool.start();
    SUBCASE("getNextLoop()")
@@ -17,7 +18,7 @@ TEST_CASE("test EventLoopThreadPool")
          threadPool.wait();
          ELG_INFO("--------------end of getNextLoop() test--------------");
       });
-      for (int i = 0; i < 8; i++)
+      for (int i = 0; i < s_thread_num; i++)
       {
          auto* loop = threadPool.getNextLoop();
          loop->setContext(0);
@@ -64,7 +65,7 @@ TEST_CASE("test EventLoopThreadPool")
          threadPool.wait();
          ELG_INFO("--------------end of size() test--------------");
       });
-      CHECK_EQ(threadPool.size(), 8);
+      CHECK_EQ(threadPool.size(), s_thread_num);
       for (auto&& loop : threadPool.getLoops()) { loop->quit(); }
    }
    SUBCASE("getLoop()")
@@ -75,7 +76,7 @@ TEST_CASE("test EventLoopThreadPool")
       });
       auto* loop      = threadPool.getLoop(0);
       loop->setContext(0);
-      loop->runEvery(1, [&](auto timerId) {
+      loop->runEvery(1, [loop, &threadPool](auto timerId) {
          auto& ctx   = loop->getContextRefMut();
          auto& count = any_cast<int&>(ctx);
          if (count == 2)
